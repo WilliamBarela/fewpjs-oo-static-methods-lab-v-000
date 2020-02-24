b117b1399325768a225fb553df699bddf9ad9a1d
William Barela <william.barela.dev@gmail.com>
2020-02-24 02:48:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 class Formatter {
   //add static methods here
   static capitalize(word){
-    return word[0].toUpperCase + word.slice(1);
+    return word[0].toUpperCase() + word.slice(1);
   }
   
   static sanitize(word){
